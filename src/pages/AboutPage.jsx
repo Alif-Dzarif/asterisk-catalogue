8387d36240af5cfc20c995a16411d7ff31f156ab
Alif-Dzarif <abdulaode9@gmail.com>
2024-02-25 06:32:44 +0700

    style: add new image

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 
-import capsPreview from '../assets/capsPreview.png'
 
+import capsPreview from '../assets/capsPreview.png'
+import AlifDzarif from '../assets/alifDzarif.png'
+import Keen from '../assets/Keen.png'
 
 export default function AboutPage() {
   return (
@@ -11,29 +13,84 @@ export default function AboutPage() {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 2 }}
-      className='py-32 grid grid-cols-3'
+      className='py-44 grid grid-cols-3'
     >
-      <div>first</div>
-      <div>
-        <div className='flex justify-center text-5xl font-bold navbar_text'>
+      <motion.div
+        initial={{ x: -1500 }}
+        animate={{ x: 0 }}
+        exit={{ x: -1500 }}
+        transition={{ duration: 1.5 }}
+        className='flex justify-center mt-16'
+      >
+        <div className='flex items-center flex-col overflow-hidden'>
+          <img src={AlifDzarif} alt="alif" className='w-52' />
+          <motion.p
+            initial={{ y: 1500 }}
+            animate={{ y: 0 }}
+            exit={{ y: 1500 }}
+            transition={{ duration: 2 }}
+            className='text-2xl font-semibold'
+          >
+            La Ode Abdul Dzarif .I
+          </motion.p>
+          <motion.p
+            initial={{ y: 1500 }}
+            animate={{ y: 0 }}
+            exit={{ y: 1500 }}
+            transition={{ duration: 2.5 }}
+          >Web Developer</motion.p>
+        </div>
+      </motion.div>
+      <motion.div
+        initial={{ y: -1500 }}
+        animate={{ y: 0 }}
+        exit={{ y: -1500 }}
+        transition={{ duration: 1 }}
+        className='py-16'
+      >
+        <div className='flex justify-center text-5xl font-medium navbar_text'>
           <h1>ABOUT ASTERISK</h1>
         </div>
-        <div className='px-10 text-lg'>
+        <div className='px-10 my-5 text-lg'>
           <p className='text-center my-5'>
-            Asterisk adalah Salah satu Design fashion yang menggabungkan susasana sporty, music, dan Astronomy
-          </p>
-          <p className='text-center my-5'>
-            Ide ini ditemukan Oleh Rakeen Ferdiansyah selaku UI/UX pada Website ini
-          </p>
-          <p className='text-center my-5'>
-            dan disisi Developer pada Website ini terdapat rekannya yaitu Laode Abdul Dzarif
+            Asterisk adalah Salah satu Design fashion yang menggabungkan susasana sporty, music, dan
           </p>
           <p className='text-center my-5'>
             Website ini akan berkembang setiap saat, dan mungkin kedepannya akan bersaing dengan nike dan adidas.
           </p>
         </div>
-      </div>
-      <div>third</div>
+        <div className='flex justify-center text-5xl font-medium navbar_text'>
+          <h1>CHEERS</h1>
+        </div>
+      </motion.div>
+      <motion.div
+        initial={{ x: 1500 }}
+        animate={{ x: 0 }}
+        exit={{ x: 1500 }}
+        transition={{ duration: 1.5 }}
+        className='flex justify-center mt-16'
+      >
+        <div className='flex flex-col items-center overflow-hidden'>
+          <img src={Keen} alt="keen" className='w-64' />
+          <motion.p
+            initial={{ y: 1500 }}
+            animate={{ y: 0 }}
+            exit={{ y: 1500 }}
+            transition={{ duration: 2 }}
+            className='text-2xl font-semibold'
+            >
+              Rakeen Ferdiansyah
+          </motion.p>
+          <motion.p
+            initial={{ y: 1500 }}
+            animate={{ y: 0 }}
+            exit={{ y: 1500 }}
+            transition={{ duration: 2.5 }}
+          >
+            Web Designer
+          </motion.p>
+        </div>
+      </motion.div>
     </motion.div>
   )
 }
